d6ec64e08936981215bab019bf46c279d050d1d9
정다움 <jeongdaum@jeongdaum-ui-MacBook-Pro.local>
2021-11-10 15:02:52 +0900

    feat: 대시보드 레이아웃 구현

--- app/views/routes.py
+++ app/views/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from app.models import Users
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def main():
-  return "hello"
+  return render_template('home.html')
+
+@bp.route("/report")
+def summary():
+  return render_template('summary.html')
